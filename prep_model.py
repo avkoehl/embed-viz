90974351a0e0fa00512c8c1fc715fee91431eae7
Arthur Koehl <avkoehl@ucdavis.edu>
2021-06-28 22:21:44 -0700

    add dimensions to begging of file for largeVis

--- prep_model.py
+++ prep_model.py
@@ -6,8 +6,8 @@ model = Word2Vec.load("./data/full.model")
 vec = model.wv.vectors
 terms = list(model.wv.vocab.keys())
 
-np.savetxt("./temp_data/vectors.csv", vec, delimiter=",")
+dim_string = str(vec.shape[0]) + " " + str(vec.shape[1])
+
+np.savetxt("./temp_data/vectors.txt", vec, header=dim_string)
 with open('./temp_data/terms.txt', 'w') as filehandle:
         filehandle.writelines("%s\n" % term for term in terms)
-
-
